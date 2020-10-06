01bd563731ad2a6bb5de8cc13291d5afc8b0398d
James Innes <34094735+james-innes@users.noreply.github.com>
2020-10-06 10:07:44 +0100

    Remove Telemetry Provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link, withRouter } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import { createBrowserHistory } from 'history'
-import { } from "@microsoft/applicationinsights-web";
 import { ApplicationInsights, SeverityLevel } from "@microsoft/applicationinsights-web";
 import { ReactPlugin, withAITracking } from "@microsoft/applicationinsights-react-js";
 
@@ -11,7 +10,7 @@ const history = createBrowserHistory();
 const reactPlugin = new ReactPlugin();
 const ai = new ApplicationInsights({
   config: {
-    instrumentationKey: "YOUR_APPINSIGHTS_INSTRUMENTATION_KEY",
+    instrumentationKey: "63b8f2d7-2dd0-4a69-a5ac-b6b202530e12",
     loggingLevelConsole: 2,
     maxBatchInterval: 0,
     maxBatchSizeInBytes: 1000,
@@ -27,75 +26,60 @@ const ai = new ApplicationInsights({
       },
     },
   },
-});
+})
 
 ai.loadAppInsights();
 ai.trackPageView();
-
-const TelemetryProvider = withRouter(
-  withAITracking(
-    ai && ai.reactPlugin,
-    class extends Component {
-      render() {
-        return this.props.children;
-      }
-    }
-  )
-);
 
 const Foo = () => <h1>foo</h1>;
 const Bar = () => <h1>bar</h1>;
 const Baz = () => <h1>baz</h1>;
 
+const App = withAITracking(reactPlugin, () => (
+  <Router history={history}>
+    <ul>
+      <li><a href="/foo">Foo</a></li>
+      <li><a href="/bar">Bar</a></li>
+      <li><a href="/baz">Baz</a></li>
+    </ul>
+
+    <Route path="/foo" component={Foo} />
+    <Route path="/bar" component={Bar} />
+    <Route path="/baz" component={Baz} />
+
+    <button onClick={() =>
+      ai.trackException({
+        error: new Error("some error"),
+        severityLevel: SeverityLevel.Error,
+      })}>
+      Track Exception
+          </button>
+
+    <button onClick={() => ai.trackEvent({ name: "some event" })}>
+      Track Event
+          </button>
+
+    <button onClick={() =>
+      ai.trackTrace({
+        message: "some trace",
+        severityLevel: SeverityLevel.Information,
+      })}>
+      Track Trace
+      </button>
+
+    <button onClick={() => { return { field: { bar: "value" } }.wrong.missing }}>
+      Auto collect an Error
+      </button>
+
+    <button onClick={() => fetch("https://httpbin.org/status/200")}>
+      Auto collect a dependency (Fetch)
+    </button>
+  </Router >
+))
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter history={history}>
-      <TelemetryProvider>
-
-        <ul>
-          <li><a href="/foo">Foo</a></li>
-          <li><a href="/bar">Bar</a></li>
-          <li><a href="/baz">Baz</a></li>
-        </ul>
-
-        <Route path="/foo" component={Foo} />
-        <Route path="/bar" component={Bar} />
-        <Route path="/baz" component={Baz} />
-
-        <button onClick={() =>
-          ai.trackException({
-            error: new Error("some error"),
-            severityLevel: SeverityLevel.Error,
-          })}>
-          Track Exception
-          </button>
-
-        <button onClick={() => ai.trackEvent({ name: "some event" })}>
-          Track Event
-          </button>
-
-        <button onClick={() =>
-          ai.trackTrace({
-            message: "some trace",
-            severityLevel: SeverityLevel.Information,
-          })}>
-          Track Trace
-      </button>
-
-        <button onClick={() => { return { field: { bar: "value" } }.wrong.missing }}>
-          Auto collect an Error
-      </button>
-
-        <button onClick={() => fetch("https://httpbin.org/status/200")}>
-          Auto collect a dependency (Fetch)
-      </button>
-
-
-      </TelemetryProvider>
-    </BrowserRouter >
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
